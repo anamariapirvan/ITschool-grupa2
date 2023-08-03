ba641df476a3aff843a4666e04fabe67099eaf70
anamariapirvan <135953019+anamariapirvan@users.noreply.github.com>
2023-08-03 20:58:11 +0300

    Session3

---
+++ src/session3/NumericPromotion.java
@@ -0,0 +1,12 @@
+package session3;
+
+public class NumericPromotion {
+    public static void main(String[] args) {
+
+
+        int x = 5;
+        double y = 6.3;
+        double result2 = x + y;
+        System.out.println("Result " + result2);
+    }
+}
